6e8af5e9213f34c746ffdad10e0f1a441b88204d
Zee Spencer <zee@zeespencer.com>
2014-10-05 13:36:42 -0400

    Feature test signing in to an account
    This doesn't really test much, except that we have a link to sign in on
    the page. It's still an important feature so I'm leaving it for now; but
    I could be convinced to torch it.

--- spec/features/user_account_spec.rb
+++ spec/features/user_account_spec.rb
@@ -17,4 +17,23 @@ describe "User Account Spec" do
       expect(current_path).to eq(root_path)
     end
   end
+
+  describe "Logging in" do
+    it "Allows a registered user to log in" do
+      registered_user = User.create({ email: "registered-user@example.com",
+                                      password: "password" })
+
+      visit "/"
+      click_link_or_button "sign_in"
+
+      fill_in "user_email", with: "registered-user@example.com"
+      fill_in "user_password", with: "password"
+
+      click_link_or_button "Log in"
+
+      expect(page).to have_content("Signed in successfully")
+      expect(current_path).to eq(root_path)
+    end
+
+  end
 end
