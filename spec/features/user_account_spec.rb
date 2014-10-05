20c9a856558e85625609dab5b5298ec90ed1b276
Zee Spencer <zee@zeespencer.com>
2014-10-05 13:53:29 -0400

    User may sign out
    I totally forgot to use the link_to helpers in my previous links; but
    here we're telling rails that the "sign out" link should use HTTP's
    DELETE method instead of GET.
    
    Now, this doesn't really "worK" in the browser. What winds up happening
    is that rails' "unobstrusive javascript" adds a javascript event handler
    to perform the request via AJAX.
    
    Another option would be to allow destruction of the current user session
    via GET. I don't perceive this as invalidating idempotency;
    as the result is the same no matter how many times you call it (Your
    session no longer exists)
    
    While both of these things are... meh... the unobstrusive js seems the least
    problematic. I do wonder what happens on the .005% of browsers that
    don't support AJAX though.

--- spec/features/user_account_spec.rb
+++ spec/features/user_account_spec.rb
@@ -31,4 +31,21 @@ feature "User Account Spec" do
     expect(page).to have_content("Signed in successfully")
     expect(current_path).to eq(root_path)
   end
+
+  scenario "A logged in user may log out" do
+    registered_user = User.create({ email: "registered-user@example.com",
+                                    password: "password" })
+
+    visit "/"
+    click_link_or_button "sign_in"
+
+    fill_in "user_email", with: "registered-user@example.com"
+    fill_in "user_password", with: "password"
+
+    click_link_or_button "Log in"
+
+
+    click_link_or_button "sign_out"
+    expect(page).to have_content("Signed out successfully")
+  end
 end
